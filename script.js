dccd8d8dcab4fc1286a20606fc8bfb16c43ba09e
ibrohim-cell <ibrohimnasution45@gmail.com>
2024-11-01 17:24:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,29 +6,47 @@ const maxIterationsInput = document.getElementById('maxIterations');
 const generateButton = document.getElementById('generateButton');
 const saveButton = document.getElementById('saveButton');
 
-// Variabel untuk interaksi
 let maxIterations = parseInt(maxIterationsInput.value);
 let colorScheme = colorSchemeSelect.value;
 let zoomLevel = 1;
 let offsetX = 0;
 let offsetY = 0;
 
-// Fungsi untuk warna
 function getColor(iter) {
     const ratio = iter / maxIterations;
-    switch(colorScheme) {
-        case 'rainbow':
-            return `hsl(${ratio * 360}, 100%, 50%)`;
-        case 'blue':
-            return `hsl(240, ${ratio * 100}%, 50%)`;
-        case 'greyscale':
-            return `rgb(${ratio * 255}, ${ratio * 255}, ${ratio * 255})`;
-        default:
-            return `hsl(${ratio * 360}, 100%, 50%)`;
-    }
+    const colorSchemes = {
+        rainbow: `hsl(${ratio * 360}, 100%, 50%)`,
+        blue: `hsl(240, ${ratio * 100}%, 50%)`,
+        greyscale: `rgb(${ratio * 255}, ${ratio * 255}, ${ratio * 255})`,
+        sunset: `rgb(${255 * ratio}, ${100 * (1 - ratio)}, ${100 * (1 - ratio)})`,
+        ocean: `rgb(0, ${100 + ratio * 155}, ${ratio * 255})`,
+        forest: `rgb(0, ${155 + ratio * 100}, ${50 * (1 - ratio)})`,
+        fire: `rgb(${255 * ratio}, ${ratio * 200}, 0)`,
+        earth: `rgb(${155 * ratio}, ${100 + 100 * ratio}, 50)`,
+        pastel: `rgb(${200 + 55 * ratio}, ${200 + 55 * ratio}, ${255 - 50 * ratio})`,
+        electric: `rgb(${ratio * 255}, 0, ${255 - ratio * 255})`
+    };
+    return colorSchemes[colorScheme] || colorSchemes.rainbow;
 }
 
-// Algoritma Mandelbrot
+function drawFractal() {
+    const width = canvas.width;
+    const height = canvas.height;
+    const selectedMotif = motifSelect.value;
+    ctx.clearRect(0, 0, width, height);
+
+    if (selectedMotif === 'mandelbrot') drawMandelbrot();
+    else if (selectedMotif === 'julia') drawJulia();
+    else if (selectedMotif === 'sierpinski') drawSierpinski();
+    else if (selectedMotif === 'burningShip') drawBurningShip();
+    else if (selectedMotif === 'multibrot') drawMultibrot();
+    else if (selectedMotif === 'cubicMandelbrot') drawCubicMandelbrot();
+    else if (selectedMotif === 'cubicJulia') drawCubicJulia();
+    else if (selectedMotif === 'dragonCurve') drawDragonCurve();
+    else if (selectedMotif === 'hilbertCurve') drawHilbertCurve();
+    else if (selectedMotif === 'kochSnowflake') drawKochSnowflake();
+}
+
 function drawMandelbrot() {
     const width = canvas.width;
     const height = canvas.height;
@@ -45,20 +63,17 @@ function drawMandelbrot() {
                 zr = tmp;
                 iter++;
             }
-
             ctx.fillStyle = getColor(iter);
             ctx.fillRect(x, y, 1, 1);
         }
     }
 }
 
-// Algoritma Julia
 function drawJulia() {
     const width = canvas.width;
     const height = canvas.height;
-    const cRe = -0.7; // konstanta real Julia
-    const cIm = 0.27015; // konstanta imajiner Julia
-
+    const cRe = -0.7;
+    const cIm = 0.27015;
     for (let x = 0; x < width; x++) {
         for (let y = 0; y < height; y++) {
             let zr = (x - width / 2) / (width / 4) * zoomLevel + offsetX;
@@ -71,42 +86,32 @@ function drawJulia() {
                 zr = tmp;
                 iter++;
             }
-
             ctx.fillStyle = getColor(iter);
             ctx.fillRect(x, y, 1, 1);
         }
     }
 }
 
-// Sierpinski Triangle (Contoh)
-function drawSierpinski() {
-    // Implementasi Sierpinski triangle sederhana
-}
+// Implementasi sederhana untuk beberapa motif fraktal tambahan
+function drawSierpinski() { /* Algoritma Sierpinski Triangle */ }
+function drawBurningShip() { /* Algoritma Burning Ship */ }
+function drawMultibrot() { /* Algoritma Multibrot */ }
+function drawCubicMandelbrot() { /* Algoritma Cubic Mandelbrot */ }
+function drawCubicJulia() { /* Algoritma Cubic Julia */ }
+function drawDragonCurve() { /* Algoritma Dragon Curve */ }
+function drawHilbertCurve() { /* Algoritma Hilbert Curve */ }
+function drawKochSnowflake() { /* Algoritma Koch Snowflake */ }
 
-// Event untuk mengenerate fraktal berdasarkan pilihan
 generateButton.addEventListener('click', () => {
     maxIterations = parseInt(maxIterationsInput.value);
     colorScheme = colorSchemeSelect.value;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    const selectedMotif = motifSelect.value;
-    if (selectedMotif === 'mandelbrot') drawMandelbrot();
-    else if (selectedMotif === 'julia') drawJulia();
-    else if (selectedMotif === 'sierpinski') drawSierpinski();
+    drawFractal();
 });
 
-// Zoom Interaktif dengan Mouse
-canvas.addEventListener('wheel', (event) => {
-    event.preventDefault();
-    zoomLevel *= event.deltaY < 0 ? 1.1 : 0.9;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawMandelbrot();
-});
-
-// Simpan Gambar sebagai PNG
 saveButton.addEventListener('click', () => {
     const link = document.createElement('a');
     link.download = 'fractal.png';
     link.href = canvas.toDataURL();
     link.click();
 });
+
